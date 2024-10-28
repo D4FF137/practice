56c5252df2543b82108227bdc57474b150b6e042
D4FF137 <143020621+D4FF137@users.noreply.github.com>
2024-10-28 11:48:47 +0300

    Add files via upload
    первы ден кодинга уху

---
+++ practice/practice/frontend/src/components/inauth.jsx
@@ -0,0 +1,12 @@
+import {Container} from 'react-bootstrap'
+const Inauth = () =>{
+    
+    return(
+        <Container>
+            <h1>безаутенфикация</h1>
+        </Container>
+    );
+}
+
+
+export default Inauth
